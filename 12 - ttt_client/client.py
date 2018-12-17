873f33d37c43208be3b118fa3647c74e18eded22
Medic840 <jankuzilek5@seznam.cz>
2018-12-17 20:54:11 +0100

    First 12

---
+++ 12 - ttt_client/client.py
@@ -0,0 +1,137 @@
+﻿import sys
+import http.client
+import json
+import re
+import time
+
+hostname = sys.argv[1]
+port_number = int(sys.argv[2])
+
+player = 0
+currentGame = 0
+
+connection = http.client.HTTPConnection(hostname, port_number)
+
+def gameChoosing():
+	userInput = input("")
+
+	patternNew = re.compile(r"new(.*)")
+	#not working
+	patternJoin = re.compile(r"(\d)")
+	
+	if(patternNew.match(userInput)):
+		global player
+		global currentGame
+		match = patternNew.match(userInput)
+		name = match.group(1).strip()
+		connection.request("GET", "/start?name={}".format(name))
+		response = connection.getresponse()
+		decodedResponse = response.read().decode('utf-8')
+		jsonRepresentation = json.loads(decodedResponse)
+		currentGame = jsonRepresentation["id"]
+		player = 1
+		UpdateClient()
+	elif(patternJoin.match(userInput)):
+		match = patternJoin.match(userInput)
+		id = match.group(1)
+		connection.request("GET", "/update?id={}".format(id))
+		response = connection.getresponse()
+		if(response.code == 200):
+			currentGame = id
+			player = 2
+			UpdateClient()
+		else:
+			print("invalid input")
+			gameChoosing()
+	else:
+		print("invalid input")
+		gameChoosing()
+		
+def PrintMark(number):
+	if(number == 0):
+		return "_"
+	elif(number == 1):
+		return "x"
+	elif(number == 2):
+		return "o"
+
+def UpdateClient():
+	connection.request("GET", "/status?game={}".format(currentGame))
+	response = connection.getresponse()
+	decodedResponse = response.read().decode('utf-8')
+	js = json.loads(decodedResponse)
+	if("winner" in js):
+		if(js["winner"] == 0):
+			print("draw")
+		elif(js["winner"] == player):
+			print("you win")
+		else:
+			print("you lose")
+		return
+		
+	print(str(PrintMark(js["board"][0][0])) + str(PrintMark(js["board"][0][1])) + str(PrintMark(js["board"][0][2])))
+	print(str(PrintMark(js["board"][1][0])) + str(PrintMark(js["board"][1][1])) + str(PrintMark(js["board"][1][2])))
+	print(str(PrintMark(js["board"][2][0])) + str(PrintMark(js["board"][2][1])) + str(PrintMark(js["board"][2][2])))
+	
+	if(js["next"] == player):
+		inGame()
+	else:
+		print("waiting for the other player")
+		currentResponse = js
+		running= True
+		while running:
+			connection.request("GET", "/status?game={}".format(currentGame))
+			response = connection.getresponse()
+			decodedResponse = response.read().decode('utf-8')
+			jsr = json.loads(decodedResponse)
+			if(currentResponse.items() != jsr.items()):
+				running = False				
+			time.sleep(1)
+		UpdateClient()
+		
+def inGame():
+	inputString = ""
+	if(player == 1):
+		inputString = "your turn (x):"
+	else:
+		inputString = "your turn (o):"
+	userInput = input(inputString)
+
+	patternPlay = re.compile(r"(\d)\s(\d)")
+
+	if(patternPlay.match(userInput)):
+		match = patternPlay.match(userInput)
+		x = match.group(1)
+		y = match.group(2)
+		
+		connection.request("GET", "/play?game={}&player={}&x={}&y={}".format(currentGame,player,x,y))
+		response = connection.getresponse()
+		if(response.status != 200):
+			print("invalid input")
+			inGame()
+		decodedResponse = response.read().decode('utf-8')
+		js = json.loads(decodedResponse)
+		if("message" in js):
+			print(js["message"])
+			inGame()
+		else:
+			UpdateClient()
+	else:
+		print("invalid input")
+		inGame()
+	
+connection.request("GET", "/list")
+response = connection.getresponse()
+decodedResponse = response.read().decode('utf-8')
+listOfDictionaries = json.loads(decodedResponse)
+
+if(len(listOfDictionaries) == 0):
+	print("There are no available games, please create a new one")
+else:
+	for dict in listOfDictionaries:
+		print(str(dict["id"]) + " " + str(dict["name"]))
+
+gameChoosing()
+inGame()
+					
+connection.close()
---
+++ 12 - ttt_client/ttt.py
@@ -0,0 +1,209 @@
+﻿import sys
+import json
+import re
+from http import HTTPStatus
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from socketserver import ThreadingMixIn
+
+port_number = int(sys.argv[1])
+dictionaryOfGames = dict()
+
+class Game:
+	def __init__(self, name, id):
+		self.id = id
+		self.name = name
+		self.nextPlayer = 1
+		self.board = [[0, 0, 0],\
+					  [0, 0, 0],\
+					  [0, 0, 0]]
+		self.winner = None
+		self.available = True
+
+	def getNextPlayer(self):
+		if(self.nextPlayer == 1):
+			return 2
+		else:
+			return 1
+
+	def mark(self,x, y):
+		self.board[x][y] = self.nextPlayer
+		self.whoWon()
+		self.nextPlayer = self.getNextPlayer()
+		return
+
+	def whoWon(self):
+		if self.winner != None:
+			return
+
+		if self.board[0][0] == self.board[1][1] == self.board[2][2] != 0:
+			self.winner = self.board[0][0]
+			return
+		if self.board[0][2] == self.board[1][1] == self.board[2][0] != 0:
+			self.winner = self.board[0][2]
+			return
+
+		for i in range(3):
+			if self.board[i][0] == self.board[i][1] == self.board[i][2] != 0:
+				self.winner = self.board[i][0]
+				return
+				
+		for j in range(3):
+			if self.board[0][j] == self.board[1][j] == self.board[2][j] != 0:
+				self.winner = self.board[0][j]
+				return
+		
+		hasEmptySpot = False
+		
+		for smallList in self.board:
+			for value in smallList:
+				if(value == 0):
+					hasEmptySpot = True
+					
+		if(hasEmptySpot == False):
+			self.winner = 0
+
+def StartGame(name):
+	gameId = len(dictionaryOfGames) + 1
+	dictionaryOfGames[gameId] = Game(name, gameId)
+	return gameId
+
+class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
+	pass
+	
+
+class myHandler(BaseHTTPRequestHandler):
+	def do_GET(self):
+		code, response = self.get_response()
+		self.send_response(code)
+		self.send_header("Content-type", "application/json")
+		#self.send_header( "Connection", "close")
+		self.end_headers()
+		self.wfile.write(response)
+		return
+			
+	def get_response(self):
+		splittedRequest = self.path[1:].split('?')
+		
+		if splittedRequest[0] not in ("start", "status", "play", "list", "update"):
+			return HTTPStatus.BAD_REQUEST, bytes(json.dumps({}), "utf-8")
+		
+		if splittedRequest[0] == "update":
+			id = int(splittedRequest[1].split("=")[1])
+			if(id not in dictionaryOfGames):
+				return HTTPStatus.BAD_REQUEST, bytes(json.dumps({}), "utf-8")
+			if(dictionaryOfGames[id].available == False):
+				return HTTPStatus.BAD_REQUEST, bytes(json.dumps({}), "utf-8")
+			dictionaryOfGames[id].available = False
+			return HTTPStatus.OK, bytes(json.dumps({}), "utf-8")
+			
+		if splittedRequest[0] == "list":
+			listToReturn = []
+			for key, value in dictionaryOfGames.items():
+				if(value.available == True):
+					listToReturn.append({"name" : value.name, "id" : value.id})				
+			return HTTPStatus.OK, bytes(json.dumps(listToReturn), "utf-8")
+			
+		if splittedRequest[0] == "start":
+			name = ""
+			
+			if(len(splittedRequest) == 1):
+				name = ""
+			else:
+				arguments = splittedRequest[1].split('&')
+				
+				pattern = re.compile(r"name=(.*)")
+				for argument in arguments:
+					match = pattern.match(argument)
+					if match != None:
+						name = match.group(1)
+						break
+			
+			gameId = StartGame(name)
+			return HTTPStatus.OK, bytes(json.dumps({"id": gameId}, indent=1), "utf-8")
+
+		if len(splittedRequest) != 2:
+			return HTTPStatus.BAD_REQUEST, bytes(json.dumps({}), "utf-8")
+		
+		if splittedRequest[0] == "status":
+			arguments = splittedRequest[1].split('&')
+			
+			gameId = -1
+			pattern = re.compile(r"game=(\d)")
+			for argument in arguments:
+				match = pattern.match(argument)
+				if match != None:
+					gameId = int(match.group(1))
+					break
+			
+			if(gameId not in dictionaryOfGames):
+				return HTTPStatus.BAD_REQUEST, bytes(json.dumps({}), "utf-8")
+				
+			game = dictionaryOfGames[gameId]
+			result = {}
+
+			if game.winner is not None:
+				result["winner"] = game.winner
+			else:
+				result["board"] = game.board
+				result["next"] = game.nextPlayer
+				
+			return HTTPStatus.OK, bytes(json.dumps(result, indent = 4), "utf-8")
+
+		if splittedRequest[0] == "play":
+			arguments = splittedRequest[1].split('&')
+			
+			gameId = -1
+			player = -1
+			x = -1
+			y = -1
+			
+			patternGame = re.compile(r"game=(\d)")
+			patternPlayer = re.compile(r"player=(\d)")
+			patternX = re.compile(r"x=(\d)")
+			patternY = re.compile(r"y=(\d)")
+			
+			for argument in arguments:
+				matchGame = patternGame.match(argument)
+				if matchGame != None:
+					gameId = int(matchGame.group(1))
+				
+				matchPlayer = patternPlayer.match(argument)
+				if matchPlayer != None:
+					player = int(matchPlayer.group(1))
+				
+				matchX = patternX.match(argument)
+				if matchX != None:
+					x = int(matchX.group(1))
+					
+				matchY = patternY.match(argument)
+				if matchY != None:
+					y = int(matchY.group(1))
+			
+			if(gameId == -1 or player == -1 or x == -1 or y == -1):
+				return HTTPStatus.BAD_REQUEST, bytes(json.dumps({}), "utf-8")
+			
+			if(gameId not in dictionaryOfGames):
+				return HTTPStatus.BAD_REQUEST, bytes(json.dumps({}), "utf-8")
+				
+			game = dictionaryOfGames[gameId]
+					
+			if game.winner is not None:
+				return HTTPStatus.OK, bytes(json.dumps({"status": "bad", "message": "Game has ended"}, indent=1), "utf-8")
+				
+			if player != game.nextPlayer:
+				return HTTPStatus.OK, bytes(json.dumps({"status": "bad", "message": "It is not your turn"}, indent=1), "utf-8")
+				
+			if x < 0 or x > 2:
+				return HTTPStatus.OK, bytes(json.dumps({"status": "bad", "message": "Wrong coordinates of x"}, indent=1),"utf-8")
+			
+			if y < 0 or y > 2:
+				return HTTPStatus.OK, bytes(json.dumps({"status": "bad", "message": "Wrong coordinates of y"}, indent=1),"utf-8")
+			
+			if game.board[x][y] != 0:
+				return HTTPStatus.OK, bytes(json.dumps({"status": "bad", "message": "This field is already taken"}, indent=1), "utf-8")
+			
+			game.mark(x,y)
+			return HTTPStatus.OK, bytes(json.dumps({"status": "ok"}, indent=1), "utf-8")
+
+server = ThreadedHTTPServer(('', port_number), myHandler)			
+server.serve_forever()
